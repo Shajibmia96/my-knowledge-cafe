8c7a5d13e210bf8f50cbac87a2dc79caec6baf27
Shajib96 <shajibahamedornob@gmail.com>
2025-02-05 20:03:29 +0600

    show data

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,43 @@
+import PropTypes from "prop-types";
+
 const Blog = ({blog}) => {
-    const {author} = blog
+    const {author, title,cover,posted_date,author_img,hashtags,reading_time} = blog
     return (
-        <div>
-            <h1>{author}</h1>
+        <div> 
+            <  div className="card w-[800px] bg-base-100 shadow-2xl mb-4">
+                <img src={cover} alt="Shoes" />
+                <div className="card-body ">
+                     <div className="flex justify-between">
+                         <div className="flex gap-6 items-end">
+                              <img className="w-16" src={author_img} alt="" />
+                               <div className="">
+                                   <h1 className="text-2xl">{author}</h1>
+                                   <p>Post : {posted_date}</p>
+                               </div>
+                         </div>
+                         <div className="flex gap-6 items-center">
+                                  <p>{reading_time} min read</p>
+                                  <button className="">BookMark</button>
+                         </div>
+                     </div>
+                     <h1 className="text-4xl font-extrabold">{title}</h1>
+                    
+                        <p className="">
+                        {
+                            hashtags.map((hash,idx )=> <span className="mr-4" key={idx}><a href="">#{hash}</a></span>)
+                         }
+                        </p>
+
+                        <button className="text-blue-600 flex justify-start">Mark as read</button>     
+                </div>
+           </div>
         </div>
     );
 };
 
+ Blog.propTypes = {
+     blog:PropTypes.object.isRequired
+ }
+
+ 
 export default Blog;
